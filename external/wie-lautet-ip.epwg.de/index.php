5b19cf8752b037aa257a33f84cfeb7db81a1da87
René Rüthlein <unverbuggt@xn--rthlein-n2a.de>
2024-10-06 10:55:45 +0200

    external php script to get ip address and other information

---
+++ external/wie-lautet-ip.epwg.de/index.php
@@ -0,0 +1,47 @@
+<?php
+if (strpos($_SERVER['HTTP_REFERER'], 'https://epwg.de') !== 0
+    && strpos($_SERVER['HTTP_REFERER'], 'https://tauri.localhost') !== 0) {
+   //header('Access-Control-Allow-Origin: *', false);
+   //echo($_SERVER['HTTP_REFERER']);
+   exit();
+}
+
+require '../geoip/geoip2.phar';
+use GeoIp2\Database\Reader;
+
+if (strpos($_SERVER['HTTP_REFERER'], 'https://tauri.localhost') == 0) {
+   header('Access-Control-Allow-Origin: https://tauri.localhost', false);
+} else {
+   header('Access-Control-Allow-Origin: https://epwg.de', false);
+}
+
+if ( isset($_GET['city']) or isset($_GET['asn']) ) {
+   if ( isset($_GET['city']) ) {
+      $reader = new Reader('../geoip/GeoLite2-City.mmdb');
+      if ($_GET['city'] == "") {
+         $record = $reader->city($_SERVER["REMOTE_ADDR"]);
+      } else {
+         $record = $reader->city($_GET['city']);
+      }
+   }
+   elseif ( isset($_GET['asn']) ) {
+      $reader = new Reader('../geoip/GeoLite2-ASN.mmdb');
+      if ( $_GET['asn'] == "") {
+         $record = $reader->asn($_SERVER["REMOTE_ADDR"]);
+      } else {
+         $record = $reader->asn($_GET['asn']);
+      }
+   }
+   header('Content-type: application/json');
+   //if ( isset($_GET['city']) ) {
+   //readfile("test2.json");
+   //} else {
+   echo json_encode($record);
+   //}
+   exit;
+} else {
+   header('Content-Type: text/plain');
+   echo $_SERVER["REMOTE_ADDR"];
+   exit;
+}
+?>
