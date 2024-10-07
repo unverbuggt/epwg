8d46ae3f268756a50ed668962b455971bf94efcf
René Rüthlein <unverbuggt@xn--rthlein-n2a.de>
2024-10-07 16:05:29 +0200

    fix icon of windows tauri app

--- pake_win.cmd
+++ pake_win.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 mkdocs build -f mkdocs_pake.yml
-call pake --name epwg --icon files\img\favicon.ico --use-local-file site\index.html
+call pake --name epwg --icon docs\img\favicon.ico --use-local-file site\index.html
 msiexec /a epwg.msi /qb targetdir="C:\temp"
 move C:\temp\PFiles\epwg\epwg.exe .
 del epwg.msi
